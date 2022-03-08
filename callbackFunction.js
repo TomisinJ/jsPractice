812005b831cfc8c6a78a398502cbd4d9170504c4
tj <tomisinjoseph3@gmail.com>
2022-03-08 10:31:03 +0000

    Process completed

--- callbackFunction.js
+++ callbackFunction.js
@@ -31,7 +31,27 @@ let production = () => {
       console.log("the food has been chopped");
 
       setTimeout(() => {
-        console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was added`)
+        console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was added`);
+
+        setTimeout(() => {
+          console.log("the machine was started");
+
+          setTimeout(() => {
+            console.log(`ice cream was placed on ${stocks.holder[0]}`);
+
+            setTimeout(() => {
+              console.log(`${stocks.toppings[0]} was added as toppings`);
+
+              setTimeout(() => {
+                console.log("serve ice cream");
+              }, 2000);
+
+            }, 3000);
+
+          }, 2000);
+
+        }, 1000);
+
       }, 1000);
 
     }, 2000);
